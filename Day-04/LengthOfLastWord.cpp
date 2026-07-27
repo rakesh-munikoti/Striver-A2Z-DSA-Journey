e0c55c1515cb760f76261771f4175b18e41b7525
RAKESH <rakeshmunikoti04@gmail.com>
2026-07-27 18:41:50 +0530

    Add solution for Length of Last Word problem
    Implemented a solution to find the length of the last word in a string using reverse traversal. Added comments for clarity.

--- Day-04/LengthOfLastWord.cpp
+++ Day-04/LengthOfLastWord.cpp
@@ -1,1 +1,31 @@
+/*
+Problem: #58 - Length of Last Word
+Platform: LeetCode
+Difficulty: Easy
+Approach: Reverse String Traversal
 
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+
+        // Skip trailing spaces
+        while(i >= 0 && s[i] == ' '){
+            i--;
+        }
+
+        int count = 0;
+
+        // Count characters of the last word
+        while(i >= 0 && s[i] != ' '){
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+};
