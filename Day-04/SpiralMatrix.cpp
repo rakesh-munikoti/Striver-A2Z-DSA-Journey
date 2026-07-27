edd03bdfb4f972e0b0d6e31a6991d616f8f457dd
RAKESH <rakeshmunikoti04@gmail.com>
2026-07-27 18:40:59 +0530

    Add spiralOrder function for matrix traversal
    Implement spiral order traversal for a matrix.

--- Day-04/SpiralMatrix.cpp
+++ Day-04/SpiralMatrix.cpp
@@ -1,1 +1,63 @@
+/*
+Problem: Spiral Matrix
+Platform: Striver A2Z
+Difficulty: Medium
+Approach: Boundary Traversal (Top, Bottom, Left, Right)
 
+Time Complexity: O(m × n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int top = 0;
+        int bottom = matrix.size() - 1;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+
+        vector<int> result;
+        int i;
+
+        while(top <= bottom && left <= right){
+
+            // Traverse Left to Right
+            i = left;
+            while(i <= right){
+                result.push_back(matrix[top][i]);
+                i++;
+            }
+            top++;
+
+            // Traverse Top to Bottom
+            i = top;
+            while(i <= bottom){
+                result.push_back(matrix[i][right]);
+                i++;
+            }
+            right--;
+
+            // Traverse Right to Left
+            if(top <= bottom){
+                i = right;
+                while(i >= left){
+                    result.push_back(matrix[bottom][i]);
+                    i--;
+                }
+            }
+            bottom--;
+
+            // Traverse Bottom to Top
+            if(left <= right){
+                i = bottom;
+                while(i >= top){
+                    result.push_back(matrix[i][left]);
+                    i--;
+                }
+            }
+            left++;
+        }
+
+        return result;
+    }
+};
