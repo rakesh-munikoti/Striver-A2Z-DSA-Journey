c679f561ca5c3da27bd6172905cd4c8c508ddecc
RAKESH <rakeshmunikoti04@gmail.com>
2026-07-27 18:40:03 +0530

    Add matrix rotation function
    Implemented a function to rotate a matrix by 90 degrees using matrix transposition and row reversal.

--- Day-04/RotateMatrix90Degrees.cpp
+++ Day-04/RotateMatrix90Degrees.cpp
@@ -1,1 +1,28 @@
+/*
+Problem: Rotate Matrix by 90 Degrees
+Platform: Striver A2Z
+Difficulty: Medium
+Approach: Matrix Transpose + Reverse Each Row
 
+Time Complexity: O(n²)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    void rotateMatrix(vector<vector<int>>& matrix) {
+        // Transpose the matrix
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = i + 1; j < matrix[0].size(); j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for(int i = 0; i < matrix.size(); i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
