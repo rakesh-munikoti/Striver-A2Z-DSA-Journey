21ba8ea2c3d68e58475e15183b9a39a503b47965
RAKESH <rakeshmunikoti04@gmail.com>
2026-07-27 18:38:40 +0530

    Add Set Matrix Zeroes solution using hash sets
    Implemented a solution to set matrix zeroes using hash sets for rows and columns.

--- Day-04/SetMatrixZeroes.cpp
+++ Day-04/SetMatrixZeroes.cpp
@@ -1,1 +1,35 @@
+/*
+Problem: Set Matrix Zeroes
+Platform: Striver A2Z
+Difficulty: Medium
+Approach: Row & Column HashSet
 
+Time Complexity: O(m × n)
+Space Complexity: O(m + n)
+*/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        // BETTER APPROACH
+        unordered_set<int> row;
+        unordered_set<int> column;
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    row.insert(i);
+                    column.insert(j);
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                if(row.find(i) != row.end() || column.find(j) != column.end()){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
